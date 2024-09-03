8f02bfa90d416bff96c9e8dc5f1f28663e96c0e6
Gen <genesishawkeye42@gmail.com>
2024-09-03 18:13:31 -0300

    FEAT: Added animations and logo to mainheader

--- components/MainHeader/main-header.jsx
+++ components/MainHeader/main-header.jsx
@@ -1,19 +1,27 @@
 import Link from "next/link";
 import classes from "./main-header.module.scss";
+import LogoIcon from "../../icons/logo";
 
 const MainHeader = () => {
   return (
-    <nav className={classes.blue}>
-      <Link href="/">Home</Link>
-      <ul>
-        <li>
-          <Link href="/projects">Projects</Link>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-      </ul>
-    </nav>
+    <header className={classes["main-header"]}>
+      <div className={classes.logo}>
+        <Link href="/">
+          <LogoIcon />
+          <span>ilberto</span>
+        </Link>
+      </div>
+      <nav className={classes["main-navigation"]}>
+        <ul>
+          <li>
+            <Link href="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
