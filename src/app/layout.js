68a686a40b9536e1343fdf76dde9b1d4a7af3ec9
Gen <genesishawkeye42@gmail.com>
2024-09-09 17:54:01 -0300

    REFACTOR: Changed page title

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import MainHeader from "../../components/MainHeader/main-header";
 import "./globals.css";
 
 export const metadata = {
-  title: "Gilberto - Front-end Developer",
+  title: "Gilberto - Desenvolvedor Front-end",
   description: "Gilberto Front-end Developer portfolio page",
 };
 
