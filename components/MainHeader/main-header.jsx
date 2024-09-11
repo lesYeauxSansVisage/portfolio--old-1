81d0cdf54cad1a8a96d0de4354d747f6b86b6fdd
Gen <genesishawkeye42@gmail.com>
2024-09-11 18:04:17 -0300

    FEAT: Added highlight to active link

--- components/MainHeader/main-header.jsx
+++ components/MainHeader/main-header.jsx
@@ -1,12 +1,19 @@
+"use client";
+
 import Link from "next/link";
 import classes from "./main-header.module.scss";
 import LogoIcon from "../../icons/logo";
+import { usePathname } from "next/navigation";
 
 const MainHeader = () => {
+  const pathname = usePathname();
+
+  console.log(pathname);
+
   return (
     <header className={classes["main-header"]}>
       <div className={classes.logo}>
-        <Link href="/">
+        <Link href="/" className={pathname == "/" ? classes.active : undefined}>
           <LogoIcon />
           <span>ilberto</span>
         </Link>
@@ -14,10 +21,20 @@ const MainHeader = () => {
       <nav className={classes["main-navigation"]}>
         <ul>
           <li>
-            <Link href="/projects">Projects</Link>
+            <Link
+              href="/projects"
+              className={pathname == "/projects" ? classes.active : undefined}
+            >
+              Projects
+            </Link>
           </li>
           <li>
-            <Link href="/about">About</Link>
+            <Link
+              href="/about"
+              className={pathname == "/about" ? classes.active : undefined}
+            >
+              About
+            </Link>
           </li>
         </ul>
       </nav>
