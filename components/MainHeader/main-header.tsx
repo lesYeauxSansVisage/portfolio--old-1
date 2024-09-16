fb876a251e9fd9e49b06c9af0e963e135cf34e99
Gen <genesishawkeye42@gmail.com>
2024-09-16 16:53:47 -0300

    FIX: Fixed external link

---
+++ components/MainHeader/main-header.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Link from "next/link";
+import classes from "./main-header.module.scss";
+import LogoIcon from "../../icons/logo";
+import { usePathname } from "next/navigation";
+
+const MainHeader = () => {
+  const pathname = usePathname();
+
+  return (
+    <header className={classes["main-header"]}>
+      <div className={classes.logo}>
+        <Link href="/" className={pathname == "/" ? classes.active : undefined}>
+          <LogoIcon />
+          <span>ilberto</span>
+        </Link>
+      </div>
+      <nav className={classes["main-navigation"]}>
+        <ul>
+          <li>
+            <Link
+              href="/projects"
+              className={pathname == "/projects" ? classes.active : undefined}
+            >
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/about"
+              className={pathname == "/about" ? classes.active : undefined}
+            >
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainHeader;
